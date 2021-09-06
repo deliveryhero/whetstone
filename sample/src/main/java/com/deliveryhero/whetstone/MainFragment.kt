932e05a8a8fbb5d9a5b9d8271ba28d1306a98791
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-09-06 09:54:58 +0200

    Workaround for multi-binding of providers

--- sample/src/main/java/com/deliveryhero/whetstone/MainFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.deliveryhero.whetstone
-
-import androidx.fragment.app.Fragment
-import com.deliveryhero.injection.fragment.ContributesFragment
-import javax.inject.Inject
-
-@ContributesFragment
-public class MainFragment @Inject constructor() : Fragment()
