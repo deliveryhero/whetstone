56beba820d2b66a67494e09375495ccdbe49b165
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-09-09 09:46:52 +0200

    Add ViewScope

--- sample/src/main/java/com/deliveryhero/whetstone/MainApplication.kt
+++ sample/src/main/java/com/deliveryhero/whetstone/MainApplication.kt
@@ -1,11 +1,11 @@
 package com.deliveryhero.whetstone
 
 import android.app.Application
+import com.deliveryhero.whetstone.component.ApplicationComponent
+import com.deliveryhero.whetstone.component.ApplicationComponentOwner
 
-public class MainApplication : Application() {
+public class MainApplication : Application(), ApplicationComponentOwner {
 
-    override fun onCreate() {
-        Whetstone.initialize { DaggerGeneratedApplicationComponent.factory().create(this) }
-        super.onCreate()
-    }
+    override val applicationComponent: ApplicationComponent =
+        DaggerGeneratedApplicationComponent.factory().create(this)
 }
--- whetstone/src/main/java/com/deliveryhero/whetstone/component/ActivityComponent.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/component/ActivityComponent.kt
@@ -14,6 +14,7 @@ import dagger.multibindings.Multibinds
  * A Dagger component that has the lifetime of the [android.app.Activity].
  */
 public interface ActivityComponent {
+    public fun getViewComponentFactory(): ViewComponentFactory
     public fun getViewModelFactoryProducer(): ViewModelFactoryProducer
     public fun getFragmentFactory(): FragmentFactory
     public fun getAnvilInjectorMap(): AnvilInjectorMap
