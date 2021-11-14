829cf0ae2404c50f1129254104df4a645c869bb5
Kingsley Adio <adiksonline@gmail.com>
2021-11-14 21:58:56 +0100

    Update sample

--- sample/src/main/java/com/deliveryhero/whetstone/MainActivity.kt
+++ sample/src/main/java/com/deliveryhero/whetstone/MainActivity.kt
@@ -2,21 +2,27 @@ package com.deliveryhero.whetstone
 
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.deliveryhero.whetstone.injector.ContributesInjector
 import com.deliveryhero.whetstone.scope.ActivityScope
+import com.deliveryhero.whetstone.viewmodel.ViewModelFactoryProducer
+import com.deliveryhero.whetstone.viewmodel.createViewModelFactory
 import javax.inject.Inject
 
 @ContributesInjector(ActivityScope::class)
 public class MainActivity : AppCompatActivity() {
 
     @Inject
-    public lateinit var dependency: MainDependency
+    public lateinit var viewModelFactoryProducer: ViewModelFactoryProducer
+    private val viewModel by viewModels<MainViewModel> {
+        viewModelFactoryProducer.createViewModelFactory(this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Whetstone.inject(activity = this)
+        Whetstone.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, dependency.getUpdatedHelloWorld(), Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, viewModel.getHelloWorld(), Toast.LENGTH_SHORT).show()
     }
 }
