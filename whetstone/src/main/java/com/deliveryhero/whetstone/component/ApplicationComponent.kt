8af98c521b909b9019caa561e2690ab52f899277
Kingsley Adio <adiksonline@gmail.com>
2021-11-17 10:46:08 +0100

    Initial integration of ContributesSubcomponent API

--- whetstone/src/main/java/com/deliveryhero/whetstone/component/ApplicationComponent.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/component/ApplicationComponent.kt
@@ -4,14 +4,18 @@ import android.app.Application
 import com.deliveryhero.whetstone.injector.MembersInjectorMap
 import com.deliveryhero.whetstone.scope.ApplicationScope
 import com.squareup.anvil.annotations.ContributesTo
+import com.deliveryhero.whetstone.SingleIn
 import dagger.BindsInstance
+import javax.inject.Singleton
 
 /**
  * A Dagger component that has the lifetime of the [android.app.Application].
  */
+@ContributesTo(ApplicationScope::class)
+@SingleIn(ApplicationScope::class)
+@Singleton
 public interface ApplicationComponent {
     public fun getMembersInjectorMap(): MembersInjectorMap
-    public fun getActivityComponentFactory(): ActivityComponentFactory
 }
 
 /**
