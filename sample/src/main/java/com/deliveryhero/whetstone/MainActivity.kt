a4ec9a3af42ef771c639d1231420a0750b43ceb7
Kingsley Adio <adiksonline@gmail.com>
2021-11-18 10:50:25 +0100

    Update sample

--- sample/src/main/java/com/deliveryhero/whetstone/MainActivity.kt
+++ sample/src/main/java/com/deliveryhero/whetstone/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.deliveryhero.whetstone.injector.ContributesInjector
-import com.deliveryhero.whetstone.scope.ActivityScope
 import com.deliveryhero.whetstone.viewmodel.ViewModelFactoryProducer
 import com.deliveryhero.whetstone.viewmodel.createViewModelFactory
 import javax.inject.Inject
 
-@ContributesInjector(ActivityScope::class)
+@ContributesAndroidBinding
 public class MainActivity : AppCompatActivity() {
 
     @Inject
