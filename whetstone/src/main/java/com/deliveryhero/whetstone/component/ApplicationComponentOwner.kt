05eb9855cca76ea336ccb6ebdf9c731fec202a3a
Kingsley Adio <adiksonline@gmail.com>
2021-11-20 15:16:27 +0100

    Make application component Factory a nested interface

--- whetstone/src/main/java/com/deliveryhero/whetstone/component/ApplicationComponentOwner.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/component/ApplicationComponentOwner.kt
@@ -1,5 +1,12 @@
 package com.deliveryhero.whetstone.component
 
+/**
+ * Interface that should be implemented by the [android.app.Application] to supply
+ * the instance of the root [ApplicationComponent].
+ *
+ * It's strongly recommended that [applicationComponent] always returns the same
+ * instance across multiple invocations.
+ */
 public interface ApplicationComponentOwner {
 
     public val applicationComponent: ApplicationComponent
