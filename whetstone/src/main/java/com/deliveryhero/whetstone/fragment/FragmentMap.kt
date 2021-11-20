aec6edfb84f1b860917e73ffd9255df06f5955f7
Kingsley Adio <adiksonline@gmail.com>
2021-11-20 15:51:33 +0100

    Replace custom keys for fragments and viewmodels with generic ClassKey

--- whetstone/src/main/java/com/deliveryhero/whetstone/fragment/FragmentMap.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/fragment/FragmentMap.kt
@@ -3,4 +3,4 @@ package com.deliveryhero.whetstone.fragment
 import androidx.fragment.app.Fragment
 import javax.inject.Provider
 
-public typealias FragmentMap = Map<Class<out Fragment>, @JvmSuppressWildcards Provider<Fragment>>
+public typealias FragmentMap = Map<Class<*>, @JvmSuppressWildcards Provider<Fragment>>
