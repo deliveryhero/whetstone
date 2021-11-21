b178755c0993c63f40a402d39d4221e6728eb4fc
Kingsley Adio <kingsley.adio@deliveryhero.com>
2021-11-21 19:37:47 +0000

    Support application injection (#11)
    * Add support for Application injection
    
    * Update sample to showcase Application injection
    
    * Update readme to showcase Application injection

--- whetstone/src/main/java/com/deliveryhero/whetstone/component/ApplicationComponent.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/component/ApplicationComponent.kt
@@ -1,12 +1,17 @@
 package com.deliveryhero.whetstone.component
 
 import android.app.Application
+import com.deliveryhero.whetstone.injector.MembersInjectorMap
+import com.deliveryhero.whetstone.scope.ApplicationScope
+import com.squareup.anvil.annotations.ContributesTo
 import dagger.BindsInstance
 
 /**
  * A Dagger component that has the lifetime of the [android.app.Application].
  */
+@ContributesTo(ApplicationScope::class)
 public interface ApplicationComponent {
+    public fun getMembersInjectorMap(): MembersInjectorMap
     public fun getActivityComponentFactory(): ActivityComponentFactory
 }
 
