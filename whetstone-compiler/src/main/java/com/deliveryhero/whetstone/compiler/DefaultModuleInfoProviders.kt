25dbfbc8eb4757d43d95c491f57eb78dd1f9324c
Kingsley Adio <adiksonline@gmail.com>
2022-03-29 11:04:52 +0200

    Update compiler to use new Anvil APIs accordingly

--- whetstone-compiler/src/main/java/com/deliveryhero/whetstone/compiler/DefaultModuleInfoProviders.kt
+++ whetstone-compiler/src/main/java/com/deliveryhero/whetstone/compiler/DefaultModuleInfoProviders.kt
@@ -1,27 +1,23 @@
 package com.deliveryhero.whetstone.compiler
 
-import com.squareup.anvil.compiler.internal.asClassName
-import com.squareup.anvil.compiler.internal.findAnnotationArgument
-import com.squareup.anvil.compiler.internal.requireFqName
+import com.squareup.anvil.compiler.internal.reference.AnnotationReference
+import com.squareup.anvil.compiler.internal.reference.asClassName
 import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
 import com.squareup.kotlinpoet.STAR
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.asClassName
 import dagger.MembersInjector
-import org.jetbrains.kotlin.com.intellij.psi.PsiElement
-import org.jetbrains.kotlin.descriptors.ModuleDescriptor
 import org.jetbrains.kotlin.name.FqName
-import org.jetbrains.kotlin.psi.KtAnnotationEntry
 
 internal class InjectorModuleInfoProvider : ModuleInfoProvider {
     private val membersInjectorCn = MembersInjector::class.asClassName()
 
     override val supportedAnnotation = FqName("com.deliveryhero.whetstone.injector.ContributesInjector")
 
-    override fun getScope(annotation: KtAnnotationEntry, module: ModuleDescriptor): ClassName {
-        val componentScope = annotation.findAnnotationArgument<PsiElement>("scope", 0) ?: error("Scope not found")
-        return componentScope.requireFqName(module).asClassName(module)
+    override fun getScope(annotation: AnnotationReference): ClassName {
+        val componentScope = annotation.scopeOrNull() ?: error("Scope not found")
+        return componentScope.asClassName()
     }
 
     override fun getTarget(annotatedClass: ClassName) = membersInjectorCn.parameterizedBy(annotatedClass)
@@ -35,7 +31,7 @@ internal class InstanceModuleInfoProvider(
     private val baseClass: TypeName
 ) : ModuleInfoProvider {
 
-    override fun getScope(annotation: KtAnnotationEntry, module: ModuleDescriptor) = scopeCn
+    override fun getScope(annotation: AnnotationReference) = scopeCn
 
     override fun getTarget(annotatedClass: ClassName) = annotatedClass
 
