c51d2f8bfa704fdbee5bba12280d4cabb27ad0ae
Kingsley Adio <adiksonline@gmail.com>
2022-09-01 13:31:54 +0200

    Update sample to take advantage of new injectedViewModel API

--- sample/src/main/java/com/deliveryhero/whetstone/sample/MainActivity.kt
+++ sample/src/main/java/com/deliveryhero/whetstone/sample/MainActivity.kt
@@ -3,27 +3,15 @@ package com.deliveryhero.whetstone.sample
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import com.deliveryhero.whetstone.Whetstone
-import com.deliveryhero.whetstone.injector.ContributesInjector
-import com.deliveryhero.whetstone.scope.ActivityScope
-import com.deliveryhero.whetstone.viewmodel.ViewModelFactoryProducer
-import javax.inject.Inject
+import com.deliveryhero.whetstone.viewmodel.injectedViewModel
 
-@ContributesInjector(ActivityScope::class)
 public class MainActivity : AppCompatActivity() {
 
-    @Inject
-    public lateinit var viewModelFactoryProducer: ViewModelFactoryProducer
-    private val viewModel by viewModels<MainViewModel> {
-        viewModelFactoryProducer.createViewModelFactory()
-    }
-
+    private val viewModel by injectedViewModel<MainViewModel>()
     private val serviceIntent by lazy { Intent(this, MainService::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        Whetstone.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, viewModel.getHelloWorld(), Toast.LENGTH_SHORT).show()
