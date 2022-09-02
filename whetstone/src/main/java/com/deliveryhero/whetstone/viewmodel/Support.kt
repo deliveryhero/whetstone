41e20305c8822d293f84281577155f3507934407
Kingsley Adio <adiksonline@gmail.com>
2022-09-02 11:47:43 +0200

    Introduce new injectedActivityViewModel API

--- whetstone/src/main/java/com/deliveryhero/whetstone/viewmodel/Support.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/viewmodel/Support.kt
@@ -4,7 +4,9 @@ package com.deliveryhero.whetstone.viewmodel
 
 import androidx.activity.ComponentActivity
 import androidx.activity.viewModels
+import androidx.annotation.MainThread
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -27,6 +29,7 @@ import com.deliveryhero.whetstone.component.ApplicationComponent
  * This property can be accessed only after the Activity is attached to the Application,
  * and access prior to that will result in IllegalArgumentException.
  */
+@MainThread
 public inline fun <reified VM : ViewModel> ComponentActivity.injectedViewModel(
     noinline extrasProducer: (() -> CreationExtras)? = null,
 ): Lazy<VM> = viewModels(extrasProducer) {
@@ -55,10 +58,34 @@ public inline fun <reified VM : ViewModel> ComponentActivity.injectedViewModel(
  * This property can be accessed only after this Fragment is attached i.e., after
  * [Fragment.onAttach()], and access prior to that will result in IllegalArgumentException.
  */
+@MainThread
 public inline fun <reified VM : ViewModel> Fragment.injectedViewModel(
     noinline ownerProducer: () -> ViewModelStoreOwner = { this },
     noinline extrasProducer: (() -> CreationExtras)? = null,
 ): Lazy<VM> = viewModels(ownerProducer, extrasProducer) {
+    val app = requireActivity().application
+    val appComponent = Whetstone.fromApplication<ApplicationComponent>(app)
+    appComponent.getViewModelFactory()
+}
+
+/**
+ * Returns a property delegate to access parent activity's [ViewModel].
+ *
+ * ```
+ * class MyFragment : Fragment() {
+ *     val activityViewModel: MyViewModel by injectedActivityViewModel()
+ * }
+ * ```
+ *
+ * This method always uses Whetstone's [ViewModelProvider.Factory] to create a new [ViewModel].
+ *
+ * This property can be accessed only after this Fragment is attached i.e., after
+ * [Fragment.onAttach()], and access prior to that will result in IllegalArgumentException.
+ */
+@MainThread
+public inline fun <reified VM : ViewModel> Fragment.injectedActivityViewModel(
+    noinline extrasProducer: (() -> CreationExtras)? = null,
+): Lazy<VM> = activityViewModels(extrasProducer) {
     val app = requireActivity().application
     val appComponent = Whetstone.fromApplication<ApplicationComponent>(app)
     appComponent.getViewModelFactory()
