c2453ee27ce71bf8549c8d0c6b1f771a937da0b5
Kingsley Adio <adiksonline@gmail.com>
2022-09-12 12:49:59 +0200

    Add a demo integration with work manager to the sample app

--- sample/src/main/java/com/deliveryhero/whetstone/sample/MainActivity.kt
+++ sample/src/main/java/com/deliveryhero/whetstone/sample/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import com.deliveryhero.whetstone.viewmodel.injectedViewModel
 
 public class MainActivity : AppCompatActivity() {
@@ -16,10 +18,13 @@ public class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Toast.makeText(this, viewModel.getHelloWorld(), Toast.LENGTH_SHORT).show()
         startService(serviceIntent)
+        val request = OneTimeWorkRequest.from(MainWorker::class.java)
+        WorkManager.getInstance(this).enqueue(request)
     }
 
     override fun onDestroy() {
         stopService(serviceIntent)
+        WorkManager.getInstance(this).cancelAllWork()
         super.onDestroy()
     }
 }
