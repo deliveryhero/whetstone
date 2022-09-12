3b8e9f23a84aa5389a26ab9a8f4aa1cd2d9acb64
Kingsley Adio <adiksonline@gmail.com>
2022-09-12 15:04:51 +0200

    Implement work manager support (#49)
    * Introduce whetstone-worker module
    
    * Implement WhetstoneWorkerInitializer for automatic installation of whetstone worker factory
    
    * Explicitly qualify the context supplied to workers with ForScope(WorkerScope)
    
    * Add a demo integration with work manager to the sample app
    
    * Update README.md to reflect integration with workmanager

---
+++ whetstone-worker/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.squareup.anvil")
+}
+
+dependencies {
+    implementation(projects.whetstone)
+    implementation(libs.dagger)
+    api(libs.androidxWorkRuntime)
+
+    testImplementation(kotlin("test-junit"))
+    testImplementation(testFixtures(libs.anvilCompilerUtils))
+    testImplementation(projects.whetstoneCompiler)
+}
---
+++ whetstone-worker/src/main/java/com/deliveryhero/whetstone/worker/WorkerScope.kt
@@ -0,0 +1,6 @@
+package com.deliveryhero.whetstone.worker
+
+/**
+ * Scope marker class for bindings that should exist for the life of an [androidx.work.ListenableWorker].
+ */
+public class WorkerScope private constructor()
