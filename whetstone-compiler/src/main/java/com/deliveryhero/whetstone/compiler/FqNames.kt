f3fbd4540197b8ae1da0c29647d61f8c3c39f89c
Kingsley Adio <adiksonline@gmail.com>
2022-09-20 20:35:38 +0200

    Reusable compiler things

---
+++ whetstone-compiler/src/main/java/com/deliveryhero/whetstone/compiler/FqNames.kt
@@ -0,0 +1,13 @@
+package com.deliveryhero.whetstone.compiler
+
+import org.jetbrains.kotlin.name.FqName
+
+internal object FqNames {
+    @JvmField val APPLICATION_COMPONENT = FqName("com.deliveryhero.whetstone.app.ApplicationComponent")
+    @JvmField val APPLICATION_SCOPE = FqName("com.deliveryhero.whetstone.app.ApplicationScope")
+    @JvmField val CONTRIBUTES_APP = FqName("com.deliveryhero.whetstone.app.ContributesApp")
+    @JvmField val CONTRIBUTES_INJECTOR = FqName("com.deliveryhero.whetstone.injector.ContributesInjector")
+    @JvmField val CONTRIBUTES_INJECTOR_META = FqName("com.deliveryhero.whetstone.meta.ContributesInjectorMeta")
+    @JvmField val CONTRIBUTES_INSTANCE_META = FqName("com.deliveryhero.whetstone.meta.ContributesInstanceMeta")
+    @JvmField val SINGLE_IN = FqName("com.deliveryhero.whetstone.SingleIn")
+}
