f56189b62a4cb162bb98a5d9bb4122ef8780ae81
s.abdoli <siavash.abdoli@deliveryhero.com>
2022-10-18 12:48:50 +0200

    Changing GlobalAndroidComponentListener for better readability

--- whetstone/src/main/java/com/deliveryhero/whetstone/logging/GlobalAndroidComponentListener.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/logging/GlobalAndroidComponentListener.kt
@@ -1,15 +1,12 @@
 package com.deliveryhero.whetstone.logging
 
 import java.lang.IllegalStateException
+import kotlin.properties.Delegates
 
 public object GlobalAndroidComponentListener {
-    public var componentInjectionListener: ComponentInjectionListener? = null
-        set(value) {
-            if (componentInjectionListener != null && value != null) {
-                // Consider adding a wrapper around this callback if you need
-                // more than callback at same time
-                throw IllegalStateException("You are overriding an existing callback")
-            }
-            field = value
-        }
+    // Consider adding a wrapper around this listener if you need
+    // more than one listener at same time
+    public var componentInjectionListener: ComponentInjectionListener? by Delegates.observable(null) { _, old, new ->
+        if (new != null && old != null) throw IllegalStateException("You are overriding an existing listener")
+    }
 }
