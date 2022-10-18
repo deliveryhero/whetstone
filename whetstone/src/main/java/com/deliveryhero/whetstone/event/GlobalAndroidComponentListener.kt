82ae442f85240d7afd6ab098053339d2c77886c2
s.abdoli <siavash.abdoli@deliveryhero.com>
2022-10-18 13:01:47 +0200

    Add documentation for GlobalAndroidComponentListener

--- whetstone/src/main/java/com/deliveryhero/whetstone/event/GlobalAndroidComponentListener.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/event/GlobalAndroidComponentListener.kt
@@ -3,6 +3,11 @@ package com.deliveryhero.whetstone.event
 import java.lang.IllegalStateException
 import kotlin.properties.Delegates
 
+/**
+ * This Object has been created to add a listener before and after any field injections in android
+ * components(Activity, View, Application, Service) you can use it for any purpose like create trace
+ * in your application. For supported one check [com.deliveryhero.whetstone.event.InjectedComponent]
+ */
 public object GlobalAndroidComponentListener {
     // Consider adding a wrapper around this listener if you need
     // more than one listener at same time
