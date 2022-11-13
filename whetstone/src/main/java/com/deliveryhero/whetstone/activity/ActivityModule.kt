11cf2cefe9f9d1b67f358859b7251a24f21e3306
Harsh Shandilya <me@msfjarvis.dev>
2022-11-14 02:27:34 +0530

    Add an activity level context binding

--- whetstone/src/main/java/com/deliveryhero/whetstone/activity/ActivityModule.kt
+++ whetstone/src/main/java/com/deliveryhero/whetstone/activity/ActivityModule.kt
@@ -1,13 +1,21 @@
 package com.deliveryhero.whetstone.activity
 
+import android.app.Activity
+import android.content.Context
+import com.deliveryhero.whetstone.ForScope
 import com.deliveryhero.whetstone.injector.MembersInjectorMap
 import com.squareup.anvil.annotations.ContributesTo
+import dagger.Binds
 import dagger.Module
 import dagger.multibindings.Multibinds
 
 @Module
 @ContributesTo(ActivityScope::class)
 public interface ActivityModule {
+
+    @Binds
+    @ForScope(ActivityScope::class)
+    public fun Activity.bindContext(): Context
 
     @Multibinds
     public fun membersInjectors(): MembersInjectorMap
